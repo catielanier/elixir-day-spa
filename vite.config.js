26df3d8eae705058f49676b7b538b0e684dbbf86
Catie Lanier <hello@catielanier.ca>
2025-07-08 16:23:45 -0400

    Optimizes dependencies for better performance
    Ensures that specific lodash methods are pre-bundled.
    This improves initial load times by making frequently used utility functions readily available.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  optimizeDeps: {
+    include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
+  }
 })
